f2308c402903f7953e2d2b92b22f21f65c323f09
Mirdev <31503196+Mirdev@users.noreply.github.com>
2025-05-08 13:04:21 +0900

    Update Bracelet_beads_arrangement.cpp[from 24.Dec.2014]
    MSP p15, the final submitted version

--- Bracelet_beads_arrangement.cpp
+++ Bracelet_beads_arrangement.cpp
@@ -1,101 +1,101 @@
 // This program checks if selected beads can form a closed bracelet and rearranges them accordingly.
 #include <iostream>
 #include <cstring>
-#include <cctype>
 using namespace std;
 
-int main() {
-    int alphabet[26] = {0}, num, i, j;
-    char beads[100][4] = {0}, tmp[4] = {0}, yn = 'y';
-    
-    while (1) {
-        for (i = 0; i < 26; i++) alphabet[i] = 0;
-        bool s = true;
-
-        cout << "Insert the number of bead you're gonna choose (5~100): ";
-        cin >> num;
-        cin.ignore();
-
-        if (num < 5 || num > 100) {
-            cout << "Invalid input range! Try again.\n";
-            continue;
-        }
-
-        cout << "Insert two colors of each bead (" << num << " total):" << endl;
-        for (i = 0; i < num; i++) {
-            cout << "=> " << i + 1 << ". : ";
-            cin.getline(beads[i], 4);
-            beads[i][0] = tolower(beads[i][0]);
-            beads[i][2] = tolower(beads[i][2]);
-
-            if (!((beads[i][0] >= 'a' && beads[i][0] <= 'z') && (beads[i][2] >= 'a' && beads[i][2] <= 'z'))) {
-                cout << "Colors must be letters a~z. Re-enter please.\n";
-                i--; continue;
-            }
-
-            alphabet[beads[i][0] - 'a']++;
-            alphabet[beads[i][2] - 'a']++;
-        }
-
-        cout << "\nThis is YOUR BEADS you picked:\n";
-        for (i = 0; i < num; i++) {
-            cout << "[" << beads[i][0] << ", " << beads[i][2] << "] ";
-        }
-        cout << "\n\n";
-
-        for (i = 0; i < 26 && s; i++) {
-            if (alphabet[i] % 2 != 0) s = false;
-        }
-
-        if (s) {
-            cout << "It's Possible to make complete Bracelet as follows!!\n";
-
-            while (true) {
-                for (i = 1; i < num; i++) {
-                    if (beads[0][0] == beads[i][2]) {
-                        strcpy(tmp, beads[i]);
-                        strcpy(beads[i], beads[num - 1]);
-                        strcpy(beads[num - 1], tmp);
-                        break;
-                    }
-                }
-
-                for (i = 0; i < num - 2; i++) {
-                    for (j = i + 1; j < num - 1; j++) {
-                        if (beads[i][2] == beads[j][0]) {
-                            strcpy(tmp, beads[i + 1]);
-                            strcpy(beads[i + 1], beads[j]);
-                            strcpy(beads[j], tmp);
-                            break;
-                        }
-                    }
-                }
-
-                if (beads[num - 1][2] == beads[0][0]) break;
-            }
-
-            for (i = 0; i < num; i++) {
-                cout << "[" << beads[i][0] << ", " << beads[i][2] << "]";
-                if (i < num - 1) cout << "-";
-            }
-            cout << "\n\n";
-        } else {
-            cout << "It's IMPOSSIBLE to make complete Bracelet!!\n\n";
-        }
-
-        while (true) {
-            cout << "One MORE?? ;-)  (y/n): ";
-            cin >> yn;
-            cin.ignore();
-            if (yn == 'y' || yn == 'Y') {
-                cout << "\n================== New Run ==================\n\n";
-                break;
-            } else if (yn == 'n' || yn == 'N') {
-                cout << "Bye~\n";
-                return 0;
-            } else {
-                cout << "y/n ONLY!\n";
-            }
-        }
-    }
+int main(){
+	int alphabet[26] = { 0, }, num, i, j;
+	char beads[100][4] = { 0, }, tmp[4] = { 0, }, yn = 'y';
+	bool s = true;
+	while (1){
+		for (i = 0; i < 27; i++) { alphabet[i] = 0; }
+		cout << "Insert the number of bead you're gonna choose(5~100): ";
+		cin >> num;
+		fflush(stdin);
+		cout << "Insert two colors of each bead (" << num << ")" << endl;
+		for (i = 0; i < num; i++){
+			cout << "=> " << i + 1 << ". : ";
+			cin.getline(beads[i], 4);
+			fflush(stdin);
+			beads[i][0] = tolower(beads[i][0]);
+			beads[i][2] = tolower(beads[i][2]);
+			if (!((beads[i][0]<123 && beads[i][0]>96) && (beads[i][2]<123 && beads[i][2]>96))){
+				cout << "Colors are a~z, 26 only. Reinput plz" << endl;
+				i--;
+				continue;
+			}
+			for (j = 0; j < 3; j += 2){
+				alphabet[beads[i][j] - 97]++;
+			}
+		}
+		cout << "\nThis is YOUR BEADS you picked." << endl;
+		for (i = 0; i < num; i++){
+			cout << "[" << beads[i][0] << ", " << beads[i][2] << "] ";
+		}
+		cout << endl << endl;
+		for (i = 0; (i < 27) && s; i++){
+			if (alphabet[i] % 2) { s = !s; }
+		}
+		if (s){
+			cout << "It's Possible to make complete Bracelet as follows!!" << endl;
+			while (1){
+				for (i = 1; i < num; i++){
+					if (beads[0][0] == beads[i][2]){
+						strcpy_s(tmp, 4, beads[i]);
+						strcpy_s(beads[i], 4, beads[num - 1]);
+						strcpy_s(beads[num - 1], 4, tmp);
+						break;
+					}
+					else if (beads[0][0] == beads[i][0]){
+						tmp[0] = beads[i][0];
+						beads[i][0] = beads[i][2];
+						beads[i][2] = tmp[0];
+						strcpy_s(tmp, 4, beads[i]);
+						strcpy_s(beads[i], 4, beads[num - 1]);
+						strcpy_s(beads[num - 1], 4, tmp);
+						break;
+					}
+				}
+				for (i = 0; i < num - 2; i++){
+					for (j = i + 1; j < num - 1; j++){
+						if (beads[i][2] == beads[j][0]){
+							strcpy_s(tmp, 4, beads[i + 1]);
+							strcpy_s(beads[i + 1], 4, beads[j]);
+							strcpy_s(beads[j], 4, tmp);
+						}
+						else if (beads[i][2] == beads[j][2]){
+							tmp[0] = beads[j][0];
+							beads[j][0] = beads[j][2];
+							beads[j][2] = tmp[0];
+							strcpy_s(tmp, 4, beads[i + 1]);
+							strcpy_s(beads[i + 1], 4, beads[j]);
+							strcpy_s(beads[j], 4, tmp);
+						}
+					}
+				}
+				if ((beads[i + 1][2] == beads[0][0]) && (beads[i + 1][0] == beads[i][2]))
+					break;
+			}
+			for (i = 0; i < num; i++){
+				cout << "[" << beads[i][0] << ", " << beads[i][2] << "]";
+				if (i < num - 1) { cout << "-"; }
+			}
+			cout << endl << endl;
+		}
+		else
+			cout << "It's IMPOSSIBLE to make complete Bracelet!!" << endl << endl;
+		while (1){
+			cout << "One MORE?? ;-)  :  ";
+			cin >> yn;
+			if (yn == 'y' || yn == 'Y'){
+				cout << "\n=======================================================\n\n";
+				break;
+			}
+			else if (yn == 'n' || yn == 'N')
+				exit(1);
+			else
+				cout << "y/n ONLY!" << endl;
+		}
+	}
+	return 0;
 }
